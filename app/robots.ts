fb4c313449fceafb29eda929d38bb8a4bafb75d8
chinenyeonukogu <Chinenyeonukogu@yahoo.com>
2026-05-03 02:45:04 -0400

    Allow Facebook crawler in robots

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,16 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/"
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/"
+      },
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/"
+      }
+    ],
     sitemap: "https://saverateafrica.com/sitemap.xml",
     host: "https://saverateafrica.com"
   };
